62d66ebbf70b466c2aaf7f8d346892afac8670fb
Inf3rn00 <joshuadodo72@gmail.com>
2025-08-19 02:26:41 +0100

    Update Icons

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { TbWorldPause } from 'react-icons/tb'
 
 const Result = () => {
   return (
-    <div>Result</div>
+    
+    <div className="flex flex-col items-center justify-center h-screen">
+      <TbWorldPause className="text-6xl text-gray-500 mb-4" />
+      <h1 className="text-2xl font-bold">Results Page</h1>
+      <p className="text-gray-600 mt-2">This is where the results will be displayed.</p>
+      <p className="text-gray-500 mt-1">Stay tuned for updates!</p>
+    </div>
+
   )
 }
 
