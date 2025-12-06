a5756af549411bdc5e5a54db8bf5a477c854b2bf
Jimba Abdullahi Olakunle <jimbaabdullahi9@gmail.com>
2025-12-06 09:32:04 +0100

    feat: add formatDate utils

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -29,3 +29,8 @@ export const submittedTime = (timeStamp) => {
   // Returning the formatted time data
   return timeData;
 };
+
+export const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+  };
